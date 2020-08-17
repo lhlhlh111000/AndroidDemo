a44bbdec0d85c97a6e1c6f548a2e0773562ebe4c
lhlhlh111000 <zlh111000@gmail.com>
2020-08-17 17:01:13 +0800

    init commit

---
+++ app/src/main/java/com/pig/android/notification/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.pig.android.notification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_notification_1.setOnClickListener {
+            notification1()
+        }
+        btn_notification_2.setOnClickListener {
+            notification2()
+        }
+        btn_notification_3.setOnClickListener {
+            notification3()
+        }
+    }
+
+    private fun notification1() {
+        val intent = this.packageManager.getLaunchIntentForPackage(packageName)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(this, "default_channel")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setContentTitle("一个短文本通知")
+            .setContentText("只会显示一行，\n太长就会省略咯,只会显示一行，\n" +
+                    "太长就会省略咯,只会显示一行，\\n\" +\n" +
+                    "                    \"太长就会省略咯,只会显示一行，\\n\" +\n" +
+                    "                    \"太长就会省略咯,只会显示一行，\\n\" +\n" +
+                    "                    \"太长就会省略咯,只会显示一行，\\n\" +\n" +
+                    "                    \"太长就会省略咯,只会显示一行，\\n\" +\n" +
+                    "                    \"太长就会省略咯,只会显示一行，\\n\" +\n" +
+                    "                    \"太长就会省略咯")
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setContentIntent(pi)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(packageName, "notification",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        notification.setChannelId(packageName)
+
+
+        manager.notify(1, notification.build())
+    }
+
+    private fun notification2() {
+        val intent = this.packageManager.getLaunchIntentForPackage(packageName)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(this, "default_channel")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setContentTitle("一个长文本通知")
+            .setStyle(NotificationCompat.BigTextStyle().bigText("这是一个很长很长很长的消息啊，这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，" +
+                    "这是一个很长很长很长的消息啊，"))
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setContentIntent(pi)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(packageName, "notification",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        notification.setChannelId(packageName)
+
+
+        manager.notify(1, notification.build())
+    }
+
+    private fun notification3() {
+        val intent = this.packageManager.getLaunchIntentForPackage(packageName)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(this, "default_channel")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setContentTitle("一个大图通知")
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(
+                BitmapFactory.decodeResource(resources, R.mipmap.big_img)
+            ))
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setContentIntent(pi)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(packageName, "notification",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        notification.setChannelId(packageName)
+
+
+        manager.notify(1, notification.build())
+    }
+}
