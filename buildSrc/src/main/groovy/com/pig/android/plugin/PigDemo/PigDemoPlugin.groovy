a9a7c6fc606de0bb8e3e1d9d7e5c5485d8c21284
lhlhlh111000 <zlh111000@gmail.com>
2020-09-07 17:56:35 +0800

    .

--- buildSrc/src/main/groovy/com/pig/android/plugin/PigDemo/PigDemoPlugin.groovy
+++ buildSrc/src/main/groovy/com/pig/android/plugin/PigDemo/PigDemoPlugin.groovy
@@ -7,7 +7,18 @@ class PigDemoPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println("hello\n")
+        project.dependencies.add("implementation", "androidx.appcompat:appcompat:1.2.0")
+        project.dependencies.add("implementation", "androidx.core:core-ktx:1.3.1")
+        project.dependencies.add("implementation", "androidx.constraintlayout:constraintlayout:1.1.3")
+        project.dependencies.add("implementation", "androidx.coordinatorlayout:coordinatorlayout:1.1.0")
+        project.dependencies.add("implementation", "com.google.android.material:material:1.2.0")
+        project.dependencies.add("implementation", "androidx.recyclerview:recyclerview:1.1.0")
+        project.dependencies.add("implementation", "org.jetbrains.kotlin:kotlin-stdlib:$project.rootProject.ext.kotlin_version")
+
+
+        project.dependencies.add("testImplementation", "junit:junit:4.12")
+        project.dependencies.add("androidTestImplementation", "androidx.test.ext:junit:1.1.1")
+        project.dependencies.add("androidTestImplementation", "androidx.test.espresso:espresso-core:3.2.0")
 
     }
 }
