1da36b8d82977969149f840928ff70d03a5a9e91
lhlhlh111000 <zlh111000@gmail.com>
2020-09-07 15:02:18 +0800

    .

---
+++ buildSrc/src/main/groovy/com/pig/android/plugin/PigDemo/PigDemoPlugin.groovy
@@ -0,0 +1,12 @@
+package com.pig.android.plugin.PigDemo
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PigDemoPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
