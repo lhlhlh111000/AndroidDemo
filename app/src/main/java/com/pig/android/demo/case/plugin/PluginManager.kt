2192ee6ef35f2b839a498a4670f8ccc1ac80ff7a
lhlhlh111000 <zlh111000@gmail.com>
2020-09-11 16:14:29 +0800

    .

---
+++ app/src/main/java/com/pig/android/demo/case/plugin/PluginManager.kt
@@ -0,0 +1,13 @@
+package com.pig.android.demo.case.plugin
+
+/**
+ * Title:
+ * Description:
+ * Copyright © 2001-2020 17173. All rights reserved.
+ *
+ * @author cqt
+ * @version 2020/9/11
+ */
+class PluginManager {
+
+}
