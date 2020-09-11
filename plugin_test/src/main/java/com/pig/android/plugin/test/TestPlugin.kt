503b50227183e52e3605efb1f47d607403320c4a
lhlhlh111000 <zlh111000@gmail.com>
2020-09-11 15:14:22 +0800

    .

---
+++ plugin_test/src/main/java/com/pig/android/plugin/test/TestPlugin.kt
@@ -0,0 +1,49 @@
+package com.pig.android.plugin.test
+
+import android.app.Activity
+import android.app.Application
+import android.util.Log
+import com.pig.android.plugin.Plugin
+
+/**
+ * Title:
+ * Description:
+ * Copyright © 2001-2020 17173. All rights reserved.
+ *
+ * @author cqt
+ * @version 2020/9/11
+ */
+class TestPlugin : Plugin {
+
+    companion object {
+        val TAG = TestPlugin::class.java.simpleName
+    }
+
+    override fun initApplication(application: Application) {
+        Log.e(TAG, "initApplication")
+    }
+
+    override fun onCreate(activity: Activity) {
+        Log.e(TAG, "onCreate")
+    }
+
+    override fun onResume(activity: Activity) {
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onStart(activity: Activity) {
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onStop(activity: Activity) {
+        Log.e(TAG, "onStop")
+    }
+
+    override fun onPause(activity: Activity) {
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onDestroy(activity: Activity) {
+        Log.e(TAG, "onDestroy")
+    }
+}
